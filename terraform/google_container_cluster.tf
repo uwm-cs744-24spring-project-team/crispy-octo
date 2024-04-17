51a233ba09b7c7639b38e0157bd297b40a643b7c
Mondo <mondo.jiang@wisc.edu>
2024-04-16 22:53:28 -0500

    feat(tf): use default node pool

--- terraform/google_container_cluster.tf
+++ terraform/google_container_cluster.tf
@@ -1,12 +1,37 @@
 resource "google_container_cluster" "primary" {
   name                     = "primary-zonal"
   location                 = var.zone
-  remove_default_node_pool = true
-  initial_node_count       = 1
+  initial_node_count       = var.node_count
+  # remove_default_node_pool = true
   network                  = "projects/cs744-spring24/global/networks/default"
   subnetwork               = "projects/cs744-spring24/regions/us-central1/subnetworks/default"
   min_master_version       = var.k8s_version
 
+  node_config {
+    preemptible  = true
+    machine_type = var.machine_type
+    disk_size_gb = var.disk_size_gb
+
+    service_account = google_service_account.default.email
+    image_type      = "COS_CONTAINERD"
+    disk_type       = "pd-balanced"
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+      "https://www.googleapis.com/auth/servicecontrol",
+      "https://www.googleapis.com/auth/service.management.readonly",
+      "https://www.googleapis.com/auth/trace.append"
+    ]
+
+    shielded_instance_config {
+      enable_integrity_monitoring = false
+    }
+  }
   release_channel {
     channel = "REGULAR"
   }
--- terraform/google_container_node_pool.tf
+++ terraform/google_container_node_pool.tf
@@ -1,44 +1,45 @@
-resource "google_container_node_pool" "primary_nodes" {
-  version    = var.k8s_version
-  name       = "primary-pool"
-  cluster    = google_container_cluster.primary.name
-  location   = var.zone
-  node_count = var.node_count
-  # deletion_protection = false
+# use cluster default node pool to speed up creation
+# resource "google_container_node_pool" "primary_nodes" {
+#   version    = var.k8s_version
+#   name       = "primary-pool"
+#   cluster    = google_container_cluster.primary.name
+#   location   = var.zone
+#   node_count = var.node_count
+#   # deletion_protection = false
 
-  node_config {
-    preemptible  = true
-    machine_type = var.machine_type
-    disk_size_gb = var.disk_size_gb
+#   node_config {
+#     preemptible  = true
+#     machine_type = var.machine_type
+#     disk_size_gb = var.disk_size_gb
 
-    service_account = google_service_account.default.email
-    image_type      = "COS_CONTAINERD"
-    disk_type       = "pd-balanced"
-    metadata = {
-      disable-legacy-endpoints = "true"
-    }
+#     service_account = google_service_account.default.email
+#     image_type      = "COS_CONTAINERD"
+#     disk_type       = "pd-balanced"
+#     metadata = {
+#       disable-legacy-endpoints = "true"
+#     }
 
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/devstorage.read_only",
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-      "https://www.googleapis.com/auth/servicecontrol",
-      "https://www.googleapis.com/auth/service.management.readonly",
-      "https://www.googleapis.com/auth/trace.append"
-    ]
-    shielded_instance_config {
-      enable_integrity_monitoring = false
-    }
-  }
+#     oauth_scopes = [
+#       "https://www.googleapis.com/auth/devstorage.read_only",
+#       "https://www.googleapis.com/auth/logging.write",
+#       "https://www.googleapis.com/auth/monitoring",
+#       "https://www.googleapis.com/auth/servicecontrol",
+#       "https://www.googleapis.com/auth/service.management.readonly",
+#       "https://www.googleapis.com/auth/trace.append"
+#     ]
+#     shielded_instance_config {
+#       enable_integrity_monitoring = false
+#     }
+#   }
 
-  upgrade_settings {
-    max_surge = 1
-    strategy  = "SURGE"
-  }
+#   upgrade_settings {
+#     max_surge = 1
+#     strategy  = "SURGE"
+#   }
 
-  management {
-    auto_repair  = true
-    auto_upgrade = true
-  }
+#   management {
+#     auto_repair  = true
+#     auto_upgrade = true
+#   }
 
-}
+# }
